7ccb3b214b6fb0f399f576d8ad76abf8d0f4dc19
pittlestix <pittsign@gmail.com>
2023-08-01 19:45:52 -0500

    500 error

--- src/routes/(full-website)/+layout.svelte
+++ src/routes/(full-website)/+layout.svelte
@@ -1,9 +1,7 @@
 <script lang = 'ts'>
 	import "$lib/styles.css"
-	import { base } from "$app/paths";
+	import {base} from "$app/paths";
 </script>
-
-<!-- A message for Elise: npm run dev -->
 
 <!-- Main body -->
 
@@ -30,12 +28,12 @@
 			<a href="{base}/office-hours"><button class="button button1"><b>Office Hours</b></button></a>
 			<a href="{base}/contact-me"><button class="button button1"><b>Contact Me</b></button></a>
 		</center></p>
-
+<!-- 
 		<hr>
 
 		<p>
 			<center><i><span style="font-size: 12px">You must be logged in to your TAMU email account to access each file.</span></i></center>
-		</p>
+		</p> -->
 		
 		</td>
 		</tr>
