28692397b4416e9b662f4e01f9691d0da7f6fb24
pittlestix <pittsign@gmail.com>
2023-08-11 02:19:38 -0500

    esos movimientos sexy siempre me entretienen

--- src/routes/(full-website)/office-hours/+page.svelte
+++ src/routes/(full-website)/office-hours/+page.svelte
@@ -1,9 +1,15 @@
+<script lang="ts">
+    import {base} from "$app/paths";
+    import Modal from "$lib/modal.svelte";
+</script>
+
 <body class="slay">
     <table align="center" width="900px" cellpadding="60px">
     <tr>
     <td class="yuh">
         <p><span style="font-size: 24px"><b>Office Hours</b></span></p>
-        <p>I do not have any set office hours, so please use <b><a href="https://forms.gle/v84eUyaBw4xDTJiMA" target="_blank">this Google Form</a></b> to request an appointment. I will respond to you from my TAMU email <b><span style="color: #FF628B">pittlestix@tamu.edu</span></b>.</p>
+        <p>I have scheduled office hours on Mondays and Wednesdays from 12 P.M. to 1 P.M. in the Policy Science and Economics Library (P.S.E.L.), which is located in the southeast corner of the <a href="https://aggiemap.tamu.edu/?bldg=1608" target="_blank">Annenberg Conference Center</a>.</p>
+        <p>If this schedule does not work for you, or you prefer to meet via Zoom, please use <b><a href="https://forms.gle/v84eUyaBw4xDTJiMA" target="_blank">this Google Form</a></b> to request an appointment. I will respond to you from my TAMU email <b><span style="color: #FF628B">pittlestix@tamu.edu</span></b> with the subject line "ECON 612 Office Hours Request".</p>
     </td>
     </tr>
     </table>
