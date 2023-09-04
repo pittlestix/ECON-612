b48675ca379359d38327a3de3f957f5ab353bee9
pittlestix <140643556+pittlestix@users.noreply.github.com>
2023-09-04 18:56:53 +0000

    add blank

--- src/routes/(full-website)/+layout.svelte
+++ src/routes/(full-website)/+layout.svelte
@@ -11,13 +11,13 @@
 		<tr>
 		<td class = "yuh">
 
-		For Class
+		<!-- For Class -->
 
 		<p><center><b><span style="font-size: 24px">Today's Materials</span></b></center></p>
-    <center><a href="https://drive.google.com/file/d/1wz5OTrEJ-lmYLc2QL_MeFjBhr4Jd2VZN/view?usp=share_link"><button class="button button1"><b>Blank</b></button></a></center>
-    <center><a href="https://drive.google.com/file/d/1Oef3LHwpl4JWO2orPyyUpML2_QhqbemX/view?usp=share_link"><button class="button button1"><b>Solutions</b></button></a></center>
-    <center><a href="https://drive.google.com/file/d/1wjukdtcaBgvuYpJYXFVW85IigzVvhtoD/view?usp=share_link"><button class="button button1"><b>All Questions</b></button></a></center>
-    <center><a href="https://drive.google.com/file/d/13rx2SZzzULU6mcNRxUoEX55Sr9un_rIX/view?usp=share_link"><button class="button button1"><b>All Solutions</b></button></a></center>
+    <center><a href="https://drive.google.com/file/d/1wz5OTrEJ-lmYLc2QL_MeFjBhr4Jd2VZN/view?usp=share_link" target="_blank"><button class="button button1"><b>Blank</b></button></a></center>
+    <center><a href="https://drive.google.com/file/d/1Oef3LHwpl4JWO2orPyyUpML2_QhqbemX/view?usp=share_link" target="_blank"><button class="button button1"><b>Solutions</b></button></a></center>
+    <center><a href="https://drive.google.com/file/d/1wjukdtcaBgvuYpJYXFVW85IigzVvhtoD/view?usp=share_link" target="_blank"><button class="button button1"><b>All Questions</b></button></a></center>
+    <center><a href="https://drive.google.com/file/d/13rx2SZzzULU6mcNRxUoEX55Sr9un_rIX/view?usp=share_link" target="_blank"><button class="button button1"><b>All Solutions</b></button></a></center>
 		
 		<!-- <h1><center>ECON 612 Examples & Solutions</center></h1> -->
 		
