8013b0e89aacbb0116d569eed80904d511889f86
pittlestix <140643556+pittlestix@users.noreply.github.com>
2023-09-04 18:45:52 +0000

    mr. 305

--- src/routes/(full-website)/+layout.svelte
+++ src/routes/(full-website)/+layout.svelte
@@ -5,17 +5,17 @@
 
 <!-- Main body -->
 
-<main>
+<!-- <main>
 	<body class = "slay">
 		<table align="center" width="900px" cellpadding="60px">
 		<tr>
 		<td class = "yuh">
 		
-		<h1><center>ECON 612 Examples & Solutions</center></h1>
+		<h1><center>ECON 612 Examples & Solutions</center></h1> -->
 		
 		<!-- Navigation -->
 		
-		<p><center>
+		<!-- <p><center>
 			<a href="{base}/"><button class = "button button1"><b>Home</b></button></a>
 			<a href="{base}/unit-01"><button class="button button1"><b>Unit 1</b></button></a>
 			<a href="{base}/unit-02"><button class="button button1"><b>Unit 2</b></button></a>
@@ -43,4 +43,4 @@
 	<p></p>
 	
 	<slot/>
-</main>
+</main> -->
--- src/routes/(full-website)/+page.svelte
+++ src/routes/(full-website)/+page.svelte
@@ -4,16 +4,29 @@
 	import { base } from "$app/paths";
 </script>
 
-<body class = "slay">
+<!-- <body class = "slay">
 <table align="center" width="900px" cellpadding="60px">
 <tr>
 <td class = "yuh">
     <p><center><b><span style="font-size: 24px">Announcements</span></b></center></p>
-    <p><center>
-        <p><i>There are currently no announcements.</i></p>
-        <!-- <b><span style="color: #FF628B">NEW!</span></b> -->
-         <!-- Welcome, everyone! Our first class will take place on Tuesday, August 22, 2023. Make sure to look over the syllabus and complete any required readings before coming to class. You should have received these materials via email from Dr. Varghese.</center><br></p> -->
+    <p><center><i>There are currently no announcements.</i></center></p>
     <p><center><b><a href="{base}/announcement-archive">Previous Announcements</a></b></center></p>
+</td>
+</tr>
+</table>
+</body> -->
+
+<!-- For class -->
+
+<body class = "slay">
+<table align="center" width="900px" cellpadding="60px">
+<tr>
+<td class = "yuh">
+    <p><center><b><span style="font-size: 24px">Today's Materials</span></b></center></p>
+    <center><a href="https://drive.google.com/file/d/1wz5OTrEJ-lmYLc2QL_MeFjBhr4Jd2VZN/view?usp=share_link"><button class="button button1"><b>Blank</b></button></a></center>
+    <center><a href="https://drive.google.com/file/d/1Oef3LHwpl4JWO2orPyyUpML2_QhqbemX/view?usp=share_link"><button class="button button1"><b>Solutions</b></button></a></center>
+    <center><a href="https://drive.google.com/file/d/1wjukdtcaBgvuYpJYXFVW85IigzVvhtoD/view?usp=share_link"><button class="button button1"><b>All Questions</b></button></a></center>
+    <center><a href="https://drive.google.com/file/d/13rx2SZzzULU6mcNRxUoEX55Sr9un_rIX/view?usp=share_link"><button class="button button1"><b>All Solutions</b></button></a></center>
 </td>
 </tr>
 </table>
