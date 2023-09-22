87c688efc8bceeb346bd31503b6c3379fbca2a98
Awise Mawie <pittsign@gmail.com>
2023-09-22 12:23:54 -0500

    rename the header

--- src/routes/(full-website)/+layout.svelte
+++ src/routes/(full-website)/+layout.svelte
@@ -10,34 +10,7 @@
 		<table align="center" width="900px" cellpadding="60px">
 		<tr>
 		<td class = "yuh">
-		<p><span style = "font: 48px">The exam ends at 7pm.
-			<br>
-			Raise your hand if you have a question.</span>
-		</p>
-		<!-- <h1><center>ECON 612 Examples & Solutions</center></h1> -->
-		
-		<!-- Navigation -->
-		
-		<!-- <p><center>
-			<a href="{base}/"><button class = "button button1"><b>Home</b></button></a>
-			<a href="{base}/unit-01"><button class="button button1"><b>Unit 1</b></button></a>
-			<a href="{base}/unit-02"><button class="button button1"><b>Unit 2</b></button></a>
-			<a href="{base}/unit-03"><button class="button button1"><b>Unit 3</b></button></a>
-		</center></p>
-		
-		<hr>
-		
-		<p><center>
-			<a href="{base}/office-hours"><button class="button button1"><b>Office Hours</b></button></a>
-			<a href="{base}/contact-me"><button class="button button1"><b>Contact Me</b></button></a>
-		</center></p>
-
-		<hr>
-
-		<p>
-			<center><i><span style="font-size: 12px">You must be logged in to your TAMU email account to access each file.</span></i></center>
-		</p> -->
-		
+		<h1><center>ECON 612</center></h1>
 		</td>
 		</tr>
 		</table>
