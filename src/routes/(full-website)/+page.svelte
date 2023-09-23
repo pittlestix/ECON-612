0f26921fc3b790d7b524bfdcc4504613503614d0
Awise Mawie <pittsign@gmail.com>
2023-09-23 14:13:26 -0500

    09/26 materials

--- src/routes/(full-website)/+page.svelte
+++ src/routes/(full-website)/+page.svelte
@@ -18,16 +18,14 @@
 
 <!-- For class -->
 
-<!-- <body class = "slay">
+<body class = "slay">
 <table align="center" width="900px" cellpadding="60px">
 <tr>
 <td class = "yuh">
     <p><center><b><span style="font-size: 24px">Today's Materials</span></b></center></p>
-    <center><a href="https://drive.google.com/file/d/1wz5OTrEJ-lmYLc2QL_MeFjBhr4Jd2VZN/view?usp=share_link"><button class="button button1"><b>Blank</b></button></a></center>
-    <center><a href="https://drive.google.com/file/d/1Oef3LHwpl4JWO2orPyyUpML2_QhqbemX/view?usp=share_link"><button class="button button1"><b>Solutions</b></button></a></center>
-    <center><a href="https://drive.google.com/file/d/1wjukdtcaBgvuYpJYXFVW85IigzVvhtoD/view?usp=share_link"><button class="button button1"><b>All Questions</b></button></a></center>
-    <center><a href="https://drive.google.com/file/d/13rx2SZzzULU6mcNRxUoEX55Sr9un_rIX/view?usp=share_link"><button class="button button1"><b>All Solutions</b></button></a></center>
+    <center><a href="https://docs.google.com/document/d/1K5dLUFx7oWLXKtb7H1ANh1pUpwA7yAxM/edit?usp=share_link&ouid=107006776461693085236&rtpof=true&sd=true"><button class="button button1"><b>Solutions</b></button></a></center>
+    <center><a href="https://drive.google.com/file/d/16oNxjIcebdgCVnsz8oBNnBeQQbInAW5B/view?usp=share_link"><button class="button button1"><b>Slides</b></button></a></center>
 </td>
 </tr>
 </table>
-</body> -->
+</body>
