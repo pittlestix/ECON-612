36477ec2a569c8f637f682d30c5035f33644cba1
Awise Mawie <pittsign@gmail.com>
2023-10-10 16:10:21 -0500

    Revert "getting rid of extra code"
    This reverts commit fa4551bd7c9997dc95600e67a4969abb84839b7c.

---
+++ src/routes/(full-website)/announcement-archive/+page.svelte
@@ -0,0 +1,74 @@
+<body class="slay">
+	<table align="center" width="900px" cellpadding="60px">
+	<tr>
+	<td class="yuh">
+		<center>
+			<p><b><span style="font-size: 24px">Previous Announcements</span></b></p>
+			<p><i>There are currently no previous announcements.</i></p>
+		</center>
+		<!-- <p><b><span style="font-size: 18px">Announcement 1: Welcome</span></b></p>
+		<p><b>Added:</b> August 1, 2023<br>
+			<b>Removed:</b> August 22, 2023<br>
+			<b>Content:</b> Welcome, everyone! Our first class will take place on Tuesday, August 22, 2023. Make sure to look over the syllabus and complete any required readings before coming to class. You should have received these materials via email from Dr. Varghese.
+		</p> -->
+    </td>
+	</tr>
+	</table>
+</body>	
+
+<style>
+	.slay {
+		background-color: #000000;
+	}
+	.yuh {
+		background-color: #161616;
+	}
+	/* hr {
+		width: 45px;
+	} */
+	/* a:link {
+		color: #F4BC53;
+		background-color: transparent;
+		text-decoration: none;
+	} */
+	/* a:visited {
+		color: #F4BC53;
+		background-color: transparent;
+		text-decoration: none;
+	} */
+	/* a:hover {
+		color: #33A7BA;
+		background-color: transparent;
+		text-decoration: bold;
+	} */
+	/* a:active {
+		color: #F4BC53;
+		background-color: transparent;
+		text-decoration: bold;
+	} */
+	b {
+		font-family: Avenir Next;
+	}
+	/* .button {
+		border: none;
+		color: #FFFFFF;
+		padding: 12px 28px;
+		text-align: center;
+		text-decoration: none;
+		display: inline-block;
+		font-family: Avenir Next;
+		font-size: 16px;
+		margin: 4px 2px;
+		transition-duration: 0.4s;
+		cursor: pointer;
+	} */
+	/* .button1 {
+		background-color: #F4BC53;
+	} */
+	/* .button1:hover {
+		background-color: #33A7BA;
+	} */
+	/* .button2 {
+		background-color: #FF628B;
+	} */
+</style>
--- src/routes/(full-website)/unit-03/+page.svelte
+++ src/routes/(full-website)/unit-03/+page.svelte
@@ -38,7 +38,7 @@
     <Modal bind:showModal = {showHomework6}>
         <center>
             <p><b><span style="font-size: 24px">Homework 6</span></b></p>
-            <p>There is no content currently available. If you believe this to be a mistake, please .</p>
+            <p>There is no content currently available. If you believe this to be a mistake, please <b><a href="{base}/contact-me">contact me</a></b>.</p>
             <!-- <p>The following materials are available:</p> -->
             <!-- <a href="" target="_blank"><button class="button button1"><b>Solutions</b></button></a>
             <a href="" target="_blank"><button class="button button1"><b>Questions</b></button></a> -->
@@ -48,7 +48,7 @@
     <Modal bind:showModal = {showHomework7}>
         <center>
             <p><b><span style="font-size: 24px">Homework 7</span></b></p>
-            <p>There is no content currently available. If you believe this to be a mistake, please .</p>
+            <p>There is no content currently available. If you believe this to be a mistake, please <b><a href="{base}/contact-me">contact me</a></b>.</p>
             <!-- <p>The following materials are available:</p> -->
             <!-- <a href="" target="_blank"><button class="button button1"><b>Solutions</b></button></a>
             <a href="" target="_blank"><button class="button button1"><b>Questions</b></button></a> -->
@@ -58,7 +58,7 @@
     <Modal bind:showModal = {showExample111}>
         <center>
             <p><b><span style="font-size: 24px">Example 11.1</span></b></p>
-            <p>There is no content currently available. If you believe this to be a mistake, please .</p>
+            <p>There is no content currently available. If you believe this to be a mistake, please <b><a href="{base}/contact-me">contact me</a></b>.</p>
             <!-- <p>The following materials are available:</p> -->
             <!-- <a href="" target="_blank"><button class="button button1"><b>Solutions</b></button></a>
             <a href="" target="_blank"><button class="button button1"><b>Questions</b></button></a> -->
@@ -68,7 +68,7 @@
     <Modal bind:showModal = {showExample112}>
         <center>
             <p><b><span style="font-size: 24px">Example 11.2</span></b></p>
-            <p>There is no content currently available. If you believe this to be a mistake, please .</p>
+            <p>There is no content currently available. If you believe this to be a mistake, please <b><a href="{base}/contact-me">contact me</a></b>.</p>
             <!-- <p>The following materials are available:</p> -->
             <!-- <a href="" target="_blank"><button class="button button1"><b>Solutions</b></button></a>
             <a href="" target="_blank"><button class="button button1"><b>Questions</b></button></a> -->
@@ -78,7 +78,7 @@
     <Modal bind:showModal = {showExample122}>
         <center>
             <p><b><span style="font-size: 24px">Example 12.2</span></b></p>
-            <p>There is no content currently available. If you believe this to be a mistake, please .</p>
+            <p>There is no content currently available. If you believe this to be a mistake, please <b><a href="{base}/contact-me">contact me</a></b>.</p>
             <!-- <p>The following materials are available:</p> -->
             <!-- <a href="" target="_blank"><button class="button button1"><b>Solutions</b></button></a>
             <a href="" target="_blank"><button class="button button1"><b>Questions</b></button></a> -->
@@ -88,7 +88,7 @@
     <Modal bind:showModal = {showDiamondDybvigGameTheoreticModel}>
         <center>
             <p><b><span style="font-size: 24px">Diamond-Dybvig Game Theoretic Model</span></b></p>
-            <p>There is no content currently available. If you believe this to be a mistake, please .</p>
+            <p>There is no content currently available. If you believe this to be a mistake, please <b><a href="{base}/contact-me">contact me</a></b>.</p>
             <!-- <p>The following materials are available:</p> -->
             <!-- <a href="" target="_blank"><button class="button button1"><b>Solutions</b></button></a>
             <a href="" target="_blank"><button class="button button1"><b>Questions</b></button></a> -->
@@ -98,7 +98,7 @@
     <Modal bind:showModal = {showUnderstandingBankRuns}>
         <center>
             <p><b><span style="font-size: 24px">Understanding Bank Runs</span></b></p>
-            <p>There is no content currently available. If you believe this to be a mistake, please .</p>
+            <p>There is no content currently available. If you believe this to be a mistake, please <b><a href="{base}/contact-me">contact me</a></b>.</p>
             <!-- <p>The following materials are available:</p> -->
             <!-- <a href="" target="_blank"><button class="button button1"><b>Solutions</b></button></a>
             <a href="" target="_blank"><button class="button button1"><b>Questions</b></button></a> -->
