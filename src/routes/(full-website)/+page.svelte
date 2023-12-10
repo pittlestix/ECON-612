4bfd205c158a7c0e98aa4ce1e8f390aa27002a1f
Awise Mawie <pittsign@gmail.com>
2023-12-10 13:34:41 -0600

    eating each other is definitely somthing they do

--- src/routes/(full-website)/+page.svelte
+++ src/routes/(full-website)/+page.svelte
@@ -11,7 +11,7 @@
     <p>
         <center>
             Click <b><a href="https://canvas.tamu.edu/courses/229915/discussion_topics/1211697" target="_blank">here</a></b> to see my most recent announcement in Canvas.
-            I'm trying to link a PDF <a href="https://pittlestix.github.io/PDFs/unit-01-advantages-of-FIs.pdf">here</a>. What about now?
+            I'm trying to link a PDF <a href="https://pittlestix.github.io/ECON-612/PDFs/unit-01-advantages-of-FIs.pdf">here</a>. What about now?
         </center>
     </p>
 </td>
